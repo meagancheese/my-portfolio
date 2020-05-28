1c0e3c545d2e2c32efc738f78a0c329a97676bca
Meagan Konst <meagan.konst@gmail.com>
2020-05-28 13:50:15 -0400

    Adds random facts and a new button for some of my favorite things.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,16 +16,34 @@
  * Adds a random fact about me to the page.
  */
 function addRandomFact() {
-  const facts =
-      ['I have a scar on the back of my head from when I fell backwards out of a chair when I was in 5th grade.',
-       'I backpacked through Portugal with a Catalan Troop.', 
-       'I have 6 pets, 5 cats and 1 dog.', 
-       'I forgot I was in the orchestra pit for "South Pacific."'];
+    const facts =
+        ['I have a scar on the back of my head from when I fell backwards out of a chair when I was in 5th grade.',
+            'I backpacked through Portugal with a Catalan Troop.',
+            'I have 6 pets, 5 cats and 1 dog.',
+            'I forgot I was in the orchestra pit for "South Pacific."',
+            'I still have a baby tooth.',
+            "I've baked chocololate chip cookies for my entire highschool more than once."];
 
-  // Pick a random fact.
-  const fact = facts[Math.floor(Math.random() * facts.length)];
+    // Pick a random fact.
+    const fact = facts[Math.floor(Math.random() * facts.length)];
 
-  // Add it to the page.
-  const randomFactContainer = document.getElementById('random-fact-container');
-  randomFactContainer.innerText = fact;
+    // Add it to the page.
+    const randomFactContainer = document.getElementById('random-fact-container');
+    randomFactContainer.innerText = fact;
+}
+
+function addRandomFavorite() {
+    const favorites =
+        ['My favorite color is purple.',
+            'My favorite icecream flavor is cookie dough.',
+            'My favorite musical is "Next to Normal."',
+            'My favorite school subject was math.',
+            'My favorite song is currently "Nice to Meet Ya" by Niall Horan.'];
+    //Pick a random favorite
+    const favorite = favorites[Math.floor(Math.random() * favorites.length)];
+
+    //Add it to the page
+    const randomFavoriteContainer = document.getElementById('random-favorite-container');
+    randomFavoriteContainer.innerText = favorite;
+
 }
