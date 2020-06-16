8d3f1be7eba6f94c9ddcf960f91ae7acb4b5a6a4
Meagan Konst <meagan.konst@gmail.com>
2020-06-16 16:42:46 +0000

    Makes a more comprehensive query function that passes all tests.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -18,52 +18,70 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class FindMeetingQuery {
   
   Collection<TimeRange> timeOptions = new ArrayList<TimeRange>();
   
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    
-    List<Event> eventsList = new ArrayList<Event>();
-    eventsList.addAll(events);
-    Collections.sort(eventsList, Event.ORDER_BY_START);
-    
-    //noOptionsForTooLongOfARequest
     if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
       return Arrays.asList();
     }
     
-    //optionsForNoAttendees and noConflicts
     if (request.getAttendees().isEmpty() || events.isEmpty()) {
       return Arrays.asList(TimeRange.WHOLE_DAY);
     }
     
-    //eventSplitsRestriction
-    //Will only consider one event for now
-    if (events.size() == 1) {
-      for (Event event : events) {
-        addTimeRange(TimeRange.START_OF_DAY, event.getWhen().start());
-        addTimeRange(event.getWhen().end(), TimeRange.END_OF_DAY);
+    Set<String> allEventAttendees = new HashSet<>();
+    for (Event event : events) {
+      allEventAttendees.addAll(event.getAttendees());
+    }
+    Set<String> allRequestAttendees = new HashSet<>();
+    allRequestAttendees.addAll(request.getAttendees());
+    for (String attendee : allRequestAttendees) {
+      if (!allEventAttendees.contains(attendee)) {
+        return Arrays.asList(TimeRange.WHOLE_DAY);
       }
     }
     
-    //everyAttendeeIsConsidered
-    //For when two people each have a meeting for now
-    if (events.size() == 2) {
+    List<Event> eventsList = new ArrayList<Event>();
+    eventsList.addAll(events);
+    Collections.sort(eventsList, Event.ORDER_BY_START);
+    boolean earlyEnd = false;
+    if (TimeRange.START_OF_DAY != eventsList.get(0).getWhen().start()) {
       addTimeRange(TimeRange.START_OF_DAY, eventsList.get(0).getWhen().start());
-      addTimeRange(eventsList.get(0).getWhen().end(), eventsList.get(1).getWhen().start());
-      addTimeRange(eventsList.get(1).getWhen().end(), TimeRange.END_OF_DAY);
+    }
+    for (int i = 1; i < eventsList.size(); i++) {
+      if (eventsList.get(i - 1).getWhen().end() != eventsList.get(i).getWhen().start()) {
+        if (!eventsList.get(i - 1).getWhen().overlaps(eventsList.get(i).getWhen())) {
+          addTimeRange(eventsList.get(i - 1).getWhen().end(), eventsList.get(i).getWhen().start());
+        }
+        if (eventsList.get(i - 1).getWhen().contains(eventsList.get(i).getWhen())) {
+          if (i + 1 < eventsList.size()) {
+            addTimeRange(eventsList.get(i - 1).getWhen().end(), eventsList.get(i + 1).getWhen().start());
+          } else {
+            earlyEnd = true;
+            if (eventsList.get(eventsList.size() - 1).getWhen().end() != TimeRange.END_OF_DAY) {
+              addTimeRange(eventsList.get(i - 1).getWhen().end(), TimeRange.END_OF_DAY);
+            }
+          }
+        } 
+      } 
+    }
+    if ((eventsList.get(eventsList.size() - 1).getWhen().end() - 1 != TimeRange.END_OF_DAY) && !earlyEnd) {
+      addTimeRange(eventsList.get(eventsList.size() - 1).getWhen().end(), TimeRange.END_OF_DAY);
     }
     
-    //overlappingEvents
+    for (TimeRange meetingOption : timeOptions) {
+      if (meetingOption.duration() >= request.getDuration()) {
+        return timeOptions;
+      }
+    }
     
-    
-    // throw new UnsupportedOperationException("TODO: Implement this method.");
-    
-    //Default for now, needed a return statement.
-    return timeOptions;
+    return Arrays.asList();
   }
   
   private void addTimeRange(int start, int end) {
